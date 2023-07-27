9a9dfa5e77397dfc5a51ffca11619dc8e6b0bf8c
Dalia Alawneh <alawnehdalia340@gmail.com>
2023-07-27 15:41:24 +0300

    Add local storage for auto-saving and loading code

--- js/app.js
+++ js/app.js
@@ -1,37 +1,54 @@
-document.getElementById("htmlCode").value="<div>\n\n</div>";
-document.getElementById("cssCode").value="<style>\n\n</style>";
-document.getElementById("jsCode").value="<script>\n\n</script>";
+const htmlCode = document.getElementById("htmlCode")
+const cssCode = document.getElementById("cssCode")
+const jsCode = document.getElementById("jsCode")
+htmlCode.value = localStorage.getItem('htmlCode')??"<div>\n\n</div>";
+cssCode.value = localStorage.getItem('cssCode')??"<style>\n\n</style>";
+jsCode.value = localStorage.getItem('jsCode')??"<script>\n\n</script>";
+showPreview()
 
-function showPreview(){
-    var htmlCode = document.getElementById("htmlCode").value;
-    var cssCode = ""+document.getElementById("cssCode").value+"";
-    var jsCode = ""+document.getElementById("jsCode").value+"";
-    var frame = document.getElementById("preview-window").contentWindow.document;
+htmlCode.addEventListener('input', () => {
+    localStorage.setItem('htmlCode', htmlCode.value)
+    showPreview()
+})
+cssCode.addEventListener('input', () => {
+    localStorage.setItem('cssCode', cssCode.value)
+    showPreview()
+})
+jsCode.addEventListener('input', () => {
+    localStorage.setItem('jsCode', jsCode.value)
+    showPreview()
+})
+
+function showPreview() {
+    const htmlCode = document.getElementById("htmlCode").value;
+    const cssCode = "" + document.getElementById("cssCode").value + "";
+    const jsCode = "" + document.getElementById("jsCode").value + "";
+    console.log(document.getElementById("preview-window").contentWindow.document);
+    const frame = document.getElementById("preview-window").contentWindow.document;
     frame.open();
-    frame.write(htmlCode+cssCode+jsCode);
+    frame.write(htmlCode + cssCode + jsCode);
     frame.close();
 }
 
-function show(x){
-    document.getElementById("html").style.display="none";
-    document.getElementById("css").style.display="none";
-    document.getElementById("js").style.display="none";
-    document.getElementById("result").style.display="none";
-    document.getElementById(x).style.display="block";
+function show(x) {
+    document.getElementById("html").style.display = "none";
+    document.getElementById("css").style.display = "none";
+    document.getElementById("js").style.display = "none";
+    document.getElementById("result").style.display = "none";
+    document.getElementById(x).style.display = "block";
 }
 
-function show_all(){
-    if(window.innerWidth>=992)
-    {
-        document.getElementById("html").style.display="block";
-        document.getElementById("css").style.display="block";
-        document.getElementById("js").style.display="block";
-        document.getElementById("result").style.display="block";
+function show_all() {
+    if (window.innerWidth >= 992) {
+        document.getElementById("html").style.display = "block";
+        document.getElementById("css").style.display = "block";
+        document.getElementById("js").style.display = "block";
+        document.getElementById("result").style.display = "block";
     }
-    if(window.innerWidth<992 && document.getElementById("html").style.display=="block")
-    {
-        document.getElementById("css").style.display="none";
-        document.getElementById("js").style.display="none";
-        document.getElementById("result").style.display="none";
+    if (window.innerWidth < 992 && document.getElementById("html").style.display == "block") {
+        document.getElementById("css").style.display = "none";
+        document.getElementById("js").style.display = "none";
+        document.getElementById("result").style.display = "none";
     }
 }
+
